ed1f2dd284c8027acea98055b4118944fb6c5f0f
Vamsi Krishna Achyutuni <achyutunivk@gmail.com>
2026-03-01 17:07:27 -0500

    16: meetings setup

---
+++ src/modules/meetings/server/ui/views/meetings-view.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+import { useTRPC } from "@/trpc/client";
+import { ErrorState } from "@/components/error-state";
+import { LoadingState } from "@/components/loading-state";
+
+export const MeetingsView = () => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
+  
+  return (
+    <div>
+      {JSON.stringify(data)}
+    </div>
+  );
+};
+
+export const MeetingsViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading Meetings"
+      decription="This may take a fews econds"
+    />
+  );
+};
+
+export const MeetingsViewError = () => {
+  return (
+    <ErrorState
+      title="Error Loading Meetings"
+      decription="Something went wrong"
+    />
+  )
+}
+
+
+
+
